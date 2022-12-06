13e3d948b189630a86e844f6a2316663adb8c590
IUT BLAGNAC <noreply@univ-tlse2.fr>
2022-12-06 14:57:05 +0100

    AJout index.php

---
+++ app/php/index.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Accueil</title>
+  <link rel="stylesheet" href="style1.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="script.js"></script>
+</head>
+
+<body>
+
+  <nav>
+    <div class="haut">
+      <img src="img/logo.png" alt="logo" width="5%" height="5%" class="item">
+      <h1 class="item">LéguMania</h1>
+
+      <form action="" method="post" class="item">
+        <input type="text" placeholder="Rechercher" name="search1">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
+
+      <img src="img/panier.png" alt="image panier" class="img-panier" width="5%" height="5%">
+    </div>
+
+    <div class="bas">
+      <a href="" class="item">Nos légumes</a>
+      <a href="" class="item">Nos fruits</a>
+      <a href="" class="item">Nos compositions</a>
+      <a href="" class="item">A propos</a>
+      <input type="button" value="CONNEXION " class="item">
+    </div>
+  </nav>
+
+  <div class="baseline">
+    <h1>Bienvenue sur LéguMania!</h1>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+  </div>
+
+  <h1>Produits populaires</h1>
+
+  <div class="populaire">
+
+    <div class="produit">
+      <img src="img/pommes.png" alt="image produit">
+      <p>Cagette de pommes rouges</p>
+      <p>3,49€</p>
+    </div>
+
+    <div class="produit">
+      <img src="img/pommes.png" alt="image produit">
+      <p>Nom</p>
+      <p>Prix</p>
+    </div>
+
+    <div class="produit">
+      <img src="img/pommes.png" alt="image produit">
+      <p>Nom</p>
+      <p>Prix</p>
+    </div>
+
+  </div>
+
+  <h1>Nos produits de saison</h1>
+
+  <div class="saison">
+
+    <div class="saison-ligne">
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+
+    </div>
+
+    <div class="saison-ligne">
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+      <div class="produit">
+        <img src="img/pommes.png" alt="image produit">
+        <p>Cagette de pommes rouges</p>
+        <p>3,49€</p>
+      </div>
+
+
+    </div>
+
+    <a href="">Voir plus de produits</a>
+
+
+  </div>
+
+
+
+
+
+</body>
+
+</html>
