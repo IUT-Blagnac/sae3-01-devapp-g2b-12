4f147c41b155ec9752d4596a23be7cd6865a1de8
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2022-12-07 16:36:45 +0100

    Update header.php

--- app/php/include/header.php
+++ app/php/include/header.php
@@ -4,14 +4,14 @@
 <head>
   <meta charset="utf-8">
   <title>Accueil</title>
-  <link rel="stylesheet" href="style/styleHeader">
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <nav>
         <div class="haut">
-            <img src="img/logo.png" alt="logo" width="5%" height="5%" class="item">
+            <img src="../img/logo.png" alt="logo" width="5%" height="5%" class="item">
             <h1 class="item">LéguMania</h1>
 
             <form action="" method="post" class="item">
@@ -19,7 +19,7 @@
                 <button type="submit"><i class="fa fa-search"></i></button>
             </form>
 
-            <img src="img/panier.png" alt="image panier" class="img-panier" width="5%" height="5%">
+            <img src="../img/panier.png" alt="image panier" class="img-panier" width="5%" height="5%">
         </div>
 
         <div class="bas">
