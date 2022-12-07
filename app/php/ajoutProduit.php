0a2501cdcf0698e275b7dcb9482a51828f1f9671
IUT BLAGNAC <noreply@univ-tlse2.fr>
2022-12-07 12:06:56 +0100

    Ajout page ajoutProduit.php

--- app/php/ajoutProduit.php
+++ app/php/ajoutProduit.php
@@ -10,12 +10,12 @@
 </head>
 
 <?php
-  include("header.php");
+include("header.php");
 ?>
 
 <body>
 
-<!-- Temporaire -->
+  <!-- Temporaire -->
   <nav>
     <div class="haut">
       <img src="img/logo.png" alt="logo" width="5%" height="5%" class="item">
@@ -37,31 +37,57 @@
       <input type="button" value="CONNEXION " class="item">
     </div>
   </nav>
-<!-- -->
+  <!-- -->
 
 
-<h1>Ajouter un produit</h1>
+  <h1 class="center">Ajouter un produit</h1>
 
 
-<form method="post" action="">
+  <div class="formulaire" id="borderTop">
 
-    <div class="form-gauche">
+    <form method="post" action="">
+
+      <div class="form-gauche">
         <p>Nom du produit</p>
         <input type="text">
+
         <p>Description</p>
-        <input type="textarea">
-    </div>
+        <textarea name="description"></textarea>
+      </div>
 
-    <div class="from-droite">
+      <div class="from-droite">
+        <p>Catégorie</p>
+        <select name="categorie">
+          <option value=""></option>
+          <option value="cat1">Catégorie1</option>
+          <option value="cat2">Catégorie2</option>
+          <option value="cat3">Catégorie3</option>
+        </select>
 
-    </div>
+        <div class="ensemble">
 
-</form>
+          <div class="part">
+            <p>Taille/poids cagette</p>
+            <input type="text">
+          </div>
 
+          <div class="part">
+            <p>Prix</p>
+            <input type="number">
+          </div>
 
+        </div>
 
+        <p>Image produit</p>
+        <input type="file" name="imgProduit" accept="image/png, image/jpeg" value="choisir un fichier">
 
+      </div>
 
+    </form>
+
+    <input type="submit" value="Ajouter le produit" class="center">
+
+  </div>
 
 </body>
 
