05108b1014773b3bd025a4ca49399091e72373a4
ludovic-estival <95869465+ludovic-estival@users.noreply.github.com>
2022-12-09 15:25:37 +0100

    update compositions/fruits.php

--- app/php/fruits.php
+++ app/php/fruits.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Accueil</title>
+    <title>Nos fruits</title>
     <link rel="stylesheet" href="include/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
